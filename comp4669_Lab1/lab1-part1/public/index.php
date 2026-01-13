d24430ef24ad494ce1d266968b22c8fe32763a90
root <root@JacobT.localdomain>
2026-01-12 18:55:38 -0800

    COMP 4669 Lab 1

--- comp4669_Lab1/lab1-part1/public/index.php
+++ comp4669_Lab1/lab1-part1/public/index.php
@@ -1,4 +1,30 @@
 <?php
+    /*same code as comp4669_lab1_par2.php file*/
+
+    /*please note this file is already included in comp4669_part1*/
+    session_start();
+
+    /* Generate a new CSRF token after every GET request */
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+    }
+
+    /* Validate token on POST */
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (
+            !isset($_POST['csrf_token']) ||
+            !isset($_SESSION['csrf_token']) ||
+            !hash_equals($_SESSION['csrf_token'], $_POST['csrf_token'])
+        ) {
+            die('CSRF validation failed');
+        }
+
+        //unset after successful use
+        unset($_SESSION['csrf_token']);
+    }
+?>
+
+<?php
 
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
@@ -18,3 +44,4 @@ require __DIR__.'/../vendor/autoload.php';
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
+?>
